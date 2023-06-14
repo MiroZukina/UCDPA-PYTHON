9ba18b81ef4e17162b1a3c44ae498ecc5ff916eb
Miro Zukina <92750661+MiroZukina@users.noreply.github.com>
2023-06-14 01:05:25 +0100

    Add files via upload

---
+++ claas 1.py
@@ -0,0 +1,58 @@
+# task 1
+
+print("Welcome to Python")
+print("Welcome to Computer Science")
+print("Programming is fun")
+ 
+
+ # task 2 
+
+for _ in range(5):
+    print("Welcome to Python")
+
+
+# Task 3 
+
+display = "FUN"
+print_F = [["" for i in range(5)] for j in range(6)]
+print_U = [["" for i in range(5)] for j in range(7)]
+print_N = [["" for i in range(5)] for j in range(8)]
+
+
+#cod for F
+lF = ""
+for row in range(5):
+    for col in range(6):
+        if (col == 0 or col == 1) or ((row == 0 or row == 2) and (col < 5)):
+            lF += "F"
+        else:
+            lF += " "
+    lF += "\n"
+
+
+
+
+# Code for U
+lU = ""
+for row in range(5):
+    for col in range(7):
+        if ((col == 0 or col == 6) and (row <3)) or ((row == 3) and (col==1 or col==5)) or ((row == 4) and (col > 1 and col < 5)):
+            lU += "U"
+        else:
+            lU += " "
+    lU += "\n"
+
+lN = ""
+for row in range(5):
+    for col in range(8):
+      if (col == 0 or col == 1 or col == 6 or col == 7  ) or (row == col) :
+            lN += "N"
+      else:
+            lN += " "
+    lN += "\n"
+
+print(lF+lU+lN)
+
+
+
+
